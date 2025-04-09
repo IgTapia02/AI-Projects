c7af23e368e4a27f9348ebb110d2bd6ff62ee2e0
Ignacio Tapia <114392929+IgTapia02@users.noreply.github.com>
2025-04-09 16:54:02 +0200

    Update OnlineHillClimbMind.cs

--- OnlineHillClimbMind.cs
+++ OnlineHillClimbMind.cs
@@ -1,7 +1,3 @@
-//Carol Álvarez
-//Jose Antonio Reyes
-//Ignacio Tapia
-//Alicia Touris
 
 using Assets.Scripts.DataStructures;
 using System.Collections.Generic;
@@ -23,10 +19,10 @@ namespace Assets.Scripts.SampleMind
 
         [SerializeField] int k = 1; //Representa el nivel de capas al que vamos a poder llegar
 
-        double totaltime = 0f;  //controladores para calcular el tiempo medio de ejecucción
+        double totaltime = 0f;  //controladores para calcular el tiempo medio de ejecucciÃ³n
         int numTime = 0;
 
-        //Método para obtener el movimiento
+        //MÃ©todo para obtener el movimiento
         public override Locomotion.MoveDirection GetNextMove(BoardInfo boardInfo, CellInfo currentPos, CellInfo[] goals)
         {
             enemyBehaviourArray = boardInfo.Enemies;
@@ -36,23 +32,23 @@ namespace Assets.Scripts.SampleMind
             if (enemyBehaviourArray.Count <= 0)
             {
                 numTime++;
-                sw = Stopwatch.StartNew();// Reiniciamos el temporizador para saber el tiempo de ejecución más adelante
+                sw = Stopwatch.StartNew();// Reiniciamos el temporizador para saber el tiempo de ejecuciÃ³n mÃ¡s adelante
 
-                searchPathHillClimb(boardInfo, currentPos, goals[0]);//Llamar a la función que establece el recorrido a seguir
+                searchPathHillClimb(boardInfo, currentPos, goals[0]);//Llamar a la funciÃ³n que establece el recorrido a seguir
                 UnityEngine.Debug.Log($"LONGITUD DEL PATH: {path.Count}");//Miramos la distancia en funcion de las casillas hasta neustro objetivo
                 UnityEngine.Debug.Log($"TIME ELAPSED: {sw.Elapsed}");// Imprimir el tiempo transcurrido              
-                UnityEngine.Debug.Log($"NUMERO DE NODOS2: {openList.Count}");// Imprimir el número de nodos dentro del path a seguir
+                UnityEngine.Debug.Log($"NUMERO DE NODOS2: {openList.Count}");// Imprimir el nÃºmero de nodos dentro del path a seguir
 
             }
             else
             {
                 numTime++;
-                sw = Stopwatch.StartNew();// Reiniciamos el temporizador para saber el tiempo de ejecución más adelante
+                sw = Stopwatch.StartNew();// Reiniciamos el temporizador para saber el tiempo de ejecuciÃ³n mÃ¡s adelante
 
-                searchPathHillClimb(boardInfo, currentPos, enemyBehaviourArray[0].CurrentPosition());//Llamar a la función que establece el recorrido a seguir
+                searchPathHillClimb(boardInfo, currentPos, enemyBehaviourArray[0].CurrentPosition());//Llamar a la funciÃ³n que establece el recorrido a seguir
                 UnityEngine.Debug.Log($"LONGITUD DEL PATH: {path.Count}");//Miramos la distancia en funcion de las casillas hasta neustro objetivo
                 UnityEngine.Debug.Log($"TIME ELAPSED: {sw.Elapsed}");// Imprimir el tiempo transcurrido
-                UnityEngine.Debug.Log($"NUMERO DE NODOS1: {openList.Count}");// Imprimir el número de nodos dentro del path a seguir
+                UnityEngine.Debug.Log($"NUMERO DE NODOS1: {openList.Count}");// Imprimir el nÃºmero de nodos dentro del path a seguir
             }
 
             totaltime += sw.ElapsedMilliseconds;
@@ -75,10 +71,10 @@ namespace Assets.Scripts.SampleMind
             return Locomotion.MoveDirection.None;// Si no hay movimientos disponibles, no se devuelve movimiento
         }
 
-        // Método para realizar la búsqueda del camino utilizando el algoritmo HillClimb
+        // MÃ©todo para realizar la bÃºsqueda del camino utilizando el algoritmo HillClimb
         void searchPathHillClimb(BoardInfo boardInfo, CellInfo startPos, CellInfo targetPos)
         {
-            openList.Clear(); // Limpiar la cola "abierta" para reempezar la búsqueda
+            openList.Clear(); // Limpiar la cola "abierta" para reempezar la bÃºsqueda
             visited.Clear(); // Limpiar el diccionario de predecesores
             deepList.Clear(); // Limpiar la lista de profundidad
 
@@ -88,7 +84,7 @@ namespace Assets.Scripts.SampleMind
 
             shorterDistance = 5000f; // Establecer una distancia suficientemente alta para que siempre varie con la primera casilla
 
-            // Bucle principal del algoritmo HillClimb si la lista de abiertos no está vacía
+            // Bucle principal del algoritmo HillClimb si la lista de abiertos no estÃ¡ vacÃ­a
 
             while (openList.Count > 0 && deepList[0] != k)
             {
@@ -96,7 +92,7 @@ namespace Assets.Scripts.SampleMind
 
                     if (current == targetPos) // Si se ha encontrado el nodo meta
                     {
-                        retracePath(startPos, targetPos); // Llamar a la función que reconstruye el camino desde la posición inicial hasta el nodo meta
+                        retracePath(startPos, targetPos); // Llamar a la funciÃ³n que reconstruye el camino desde la posiciÃ³n inicial hasta el nodo meta
                         return; // Salir del bucle
                     }
 
@@ -107,7 +103,7 @@ namespace Assets.Scripts.SampleMind
                         {
                             if (deepList[0] < k) // Si la profundidad sigue siendo menor que k
                             {
-                                openList.Add(next); // Añadir el nodo vecino a la cola 'openList' que funciona como la lista "abierta"
+                                openList.Add(next); // AÃ±adir el nodo vecino a la cola 'openList' que funciona como la lista "abierta"
                                 visited[next] = current; // Almacenar el predecesor del nodo vecino
                                 deepList.Add(deepList[0] + 1); // Sumamos uno en cada nueva generacion
                             }
@@ -128,25 +124,25 @@ namespace Assets.Scripts.SampleMind
                 }
             }
 
-            retracePath(startPos, shortestNode); // Llamar a la función que reconstruye el camino desde la posición inicial hasta el nodo meta
+            retracePath(startPos, shortestNode); // Llamar a la funciÃ³n que reconstruye el camino desde la posiciÃ³n inicial hasta el nodo meta
             return; // Salir del bucle
         }
         
-        // Método para reconstruir el camino desde la posición inicial hasta el nodo meta
+        // MÃ©todo para reconstruir el camino desde la posiciÃ³n inicial hasta el nodo meta
         void retracePath(CellInfo startNode, CellInfo endNode)
         {
             path.Clear();
             CellInfo currentNode = endNode; // Comenzar desde el nodo meta
 
-            // Bucle para retroceder desde el nodo meta hasta la posición inicial
+            // Bucle para retroceder desde el nodo meta hasta la posiciÃ³n inicial
             while (currentNode != startNode)
             {
-                path.Push(currentNode); // Añadir el nodo actual a la pila 'newPath'
+                path.Push(currentNode); // AÃ±adir el nodo actual a la pila 'newPath'
                 currentNode = visited[currentNode]; // Obtener el predecesor del nodo actual en el camino
             }
         }
 
-        // Método para calcular la distancia entre dos celdas en el tablero (distancia de Manhattan)
+        // MÃ©todo para calcular la distancia entre dos celdas en el tablero (distancia de Manhattan)
         float getDistanceToEndPoint(CellInfo A, CellInfo B)
         {
             float distRows = Mathf.Abs(A.RowId - B.RowId);
